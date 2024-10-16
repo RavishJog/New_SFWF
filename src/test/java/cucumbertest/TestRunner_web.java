02c5b10f58c37fbce67a4b76acf6702b9be40e48
RavishJog <83834874+RavishJog@users.noreply.github.com>
2024-10-16 16:34:02 +0400

    Update TestRunner_web.java

--- src/test/java/cucumbertest/TestRunner_web.java
+++ src/test/java/cucumbertest/TestRunner_web.java
@@ -16,7 +16,7 @@ import org.testng.annotations.Test;
                 "pretty",
                 "json:target/cucumber-reports/cucumber.json" // Specify the JSON report path
         }
-        ,tags = "@Application_for_Launch"
+        ,tags = "@GOC_Registrations"
 )
 
 public class TestRunner_web {
